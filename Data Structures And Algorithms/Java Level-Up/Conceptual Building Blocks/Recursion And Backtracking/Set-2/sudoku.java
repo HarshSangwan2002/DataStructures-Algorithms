cef5e762f9adaac00eb77af32c96568e49c47a62
HarshSangwan2002 <hsangwan2002@gmail.com>
2021-09-08 18:25:08 +0530

    init

--- Data Structures And Algorithms/Java Level-Up/Conceptual Building Blocks/Recursion And Backtracking/Set-2/sudoku.java
+++ Data Structures And Algorithms/Java Level-Up/Conceptual Building Blocks/Recursion And Backtracking/Set-2/sudoku.java
@@ -13,6 +13,8 @@ public class sudoku {
     }
 
     public static void solveSudoku(int[][] board, int i, int j) {
+        // write yopur code here
+
         if (i == board.length) {
             display(board);
             return;
@@ -24,17 +26,21 @@ public class sudoku {
         if (j == board[0].length - 1) {
             ni = i + 1;
             nj = 0;
-        } else {
+        }
+
+        else {
             ni = i;
             nj = j + 1;
         }
 
         if (board[i][j] != 0) {
             solveSudoku(board, ni, nj);
-        } else {
-            for (int val = 1; val <= 9; val++) {
-                if (isValid(board, i, j, val)) {
-                    board[i][j] = val;
+        }
+
+        else {
+            for (int po = 1; po <= 9; po++) {
+                if (isvalid(board, i, j, po)) {
+                    board[i][j] = po;
                     solveSudoku(board, ni, nj);
                     board[i][j] = 0;
                 }
@@ -42,27 +48,24 @@ public class sudoku {
         }
     }
 
-    public static boolean isValid(int[][] board, int x, int y, int val) {
-        int n = board.length;
-        for (int i = 0; i < n; i++) {
-            if (board[x][i] == val) {
+    public static boolean isvalid(int[][] board, int x, int y, int po) {
+        for (int j = 0; j < board[0].length; j++) {
+            if (board[x][j] == po)
                 return false;
-            }
         }
 
-        for (int i = 0; i < n; i++) {
-            if (board[i][y] == val) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][y] == po)
                 return false;
-            }
         }
 
-        x = x / 3 * 3;
-        y = y / 3 * 3;
+        int smi = x / 3 * 3;
+        int smj = y / 3 * 3;
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (board[x + i][y + j] == val) {
+                if (board[smi + i][smj + j] == po)
                     return false;
-                }
             }
         }
 
